4c387245181882a3b3dc656ebf276dc9c7075d6a
sophy <2njoybliss@gmail.com>
2020-01-20 17:38:24 +0900

    20/01/20  주말 (18 ~ 19) 작업본 window&공유DB 에 맞춰 새롭게 설정진행중.

--- WATO/src/main/java/com/king/myapp/persistence/BoardDAO.java
+++ WATO/src/main/java/com/king/myapp/persistence/BoardDAO.java
@@ -3,8 +3,9 @@ package com.king.myapp.persistence;
 import java.util.List;
 
 import com.king.myapp.domain.BoardVO;
-import com.king.myapp.domain.Criteria;
-import com.king.myapp.domain.SearchCriteria;
+import com.king.myapp.domain.StudyEnrollVO;
+import com.king.myapp.domain.StudyListFilter;
+import com.king.myapp.domain.TeacherEnrollVO;
 
 
 
@@ -18,6 +19,14 @@ public interface BoardDAO {
 	public List<BoardVO> searchResultTeacher(String searchKey) throws Exception;
 
 	public List<BoardVO> searchResultQna(String searchKey) throws Exception;
+
+	public List<BoardVO> studylistAll() throws Exception;
+
+	public List<StudyEnrollVO> studylistfilter(StudyListFilter sLF) throws Exception;
+
+	public List<BoardVO> TearchlistAll() throws Exception;
+
+	public List<TeacherEnrollVO> TeacherListFilter(StudyListFilter sLF)throws Exception;
 	
 	
 }
